2cc7307d0fdc1b9b0cc2ee2be77f86ddf7e31dd9
Arilson Souza <arilsons61@gmail.com>
2020-12-29 21:49:48 -0300

    Showing a Users

--- rumbl/lib/rumbl_web/controllers/user_controller.ex
+++ rumbl/lib/rumbl_web/controllers/user_controller.ex
@@ -7,4 +7,9 @@ defmodule RumblWeb.UserController do
     users = Accounts.list_users()
     render(conn, "index.html", users: users)
   end
+
+  def show(conn, %{"id" => id}) do
+    user = id |> String.to_integer() |> Accounts.get_user()
+    render(conn, "show.html", user: user)
+  end
 end
